f1709d6aa617e61cdabe7bb752da61c0a12cae8a
imexotic <hubert.stoklosa23@gmail.com>
2024-01-26 20:05:59 +0000

    add python solution to 0167

---
+++ python/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,16 @@
+class Solution(object):
+
+    def twoSum(self, numbers, target):
+        left, right = 0, len(numbers) - 1
+
+        while (left < right):
+            curr_sum = numbers[left] + numbers[right]
+
+            if (curr_sum == target):
+                return [left + 1, right + 1]
+            elif (curr_sum < target):
+                left += 1
+            elif (curr_sum > target):
+                right -= 1
+
+        return []
