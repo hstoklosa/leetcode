99f3942daa334d80e7521419a2a3bb3028588543
hstoklosa <hubert.stoklosa23@gmail.com>
2024-10-26 00:00:00 +0100

    optimise 125-valid-palindrome

--- 2-two-pointers/125-valid-palindrome.py
+++ 2-two-pointers/125-valid-palindrome.py
@@ -1,6 +1,32 @@
 class Solution:
 
+    """
+
+    OPTIMISED SOLUTION
+      Better space utilisation by transforming the input straight 
+      into a string instead of using a list as an intermediary.
+    
+    """
     def isPalindrome(self, s: str) -> bool:
+        s, ns = s.lower(), ''
+        i, j = 0, len(s) - 1
+        while i < len(s):
+            if s[i].isalnum(): 
+                ns += s[i]
+                
+            i += 1
+
+        i, j = 0, len(ns) - 1
+        while i < j:
+            if ns[i] != ns[j]:
+                return False
+            
+            i += 1
+            j -= 1
+            
+        return True
+
+    def isPalindrome2(self, s: str) -> bool:
         s = "".join(filter(str.isalnum, s)).lower() 
         i, j = 0, len(s) - 1
 
