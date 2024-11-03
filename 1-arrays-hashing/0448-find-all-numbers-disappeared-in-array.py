406402415e097c6ba03f3670fccab85919083d9f
hstoklosa <hubert.stoklosa23@gmail.com>
2024-11-03 23:46:44 +0000

    add solution to 0448-find-all-numbers-disappeared-in-array

---
+++ 1-arrays-hashing/0448-find-all-numbers-disappeared-in-array.py
@@ -0,0 +1,18 @@
+""" 
+PROBLEM:     0448. Find All Numbers Disappeared in an Array
+             https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
+DIFFICULTY:  Easy
+PATTERN:     Arrays
+"""
+
+class Solution:
+    def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        set_nums = set(nums)
+        result = []
+
+        for n in range(1, n+1):
+            if n not in set_nums:
+                result.append(n)
+
+        return result
