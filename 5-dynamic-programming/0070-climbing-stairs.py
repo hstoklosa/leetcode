727bfb0a3e2ffedcdae2cf7204c9034d09ec9ed5
hstoklosa <hubert.stoklosa23@gmail.com>
2024-11-04 22:51:24 +0000

    modify directory order of dynamic programming solutions

--- 5-dynamic-programming/0070-climbing-stairs.py
+++ 5-dynamic-programming/0070-climbing-stairs.py
@@ -1,18 +1,18 @@
 """ 
+DIFFICULTY:  Easy
 PROBLEM:     0070. Climbing Stairs
              https://leetcode.com/problems/climbing-stairs
-DIFFICULTY:  Easy
 PATTERN:     Dynamic Programming
 """
 
 class Solution:
 
     """
-    Dynamic programming is a name given to a class of algorithms which remember solutions to previously computed subproblems in order to increase efficiency.
+    Definition: Dynamic programming is a name given to a class of algorithms which remember solutions to previously computed subproblems in order to increase efficiency.
 
     https://www.hello-algo.com/en/chapter_dynamic_programming/intro_to_dynamic_programming
+    https://www.hackerearth.com/practice/algorithms/dynamic-programming/introduction-to-dynamic-programming-1/tutorial/
     https://cp-algorithms.com/dynamic_programming/intro-to-dp.html
-    https://en.wikipedia.org/wiki/Dynamic_programming
     """
 
     def climbStairs(self, n: int) -> int:
