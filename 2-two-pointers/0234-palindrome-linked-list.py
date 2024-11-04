826237e8d5bcb2c5197053a1f6bffeccc8dc42af
hstoklosa <hubert.stoklosa23@gmail.com>
2024-11-04 16:53:38 +0000

    add optimised solution to 0234-palindrome-linked-list

--- 2-two-pointers/0234-palindrome-linked-list.py
+++ 2-two-pointers/0234-palindrome-linked-list.py
@@ -11,7 +11,57 @@ class ListNode:
         self.next = next
 
 class Solution:
+
+    """
+    The solution has been improved by:
+      1. finding the middle of the linked list and splitting it 
+      2. reversing the second half for comparison with the first half
+    
+    Optimised version runs in O(n) time and uses O(1) space.
+    """
+    
     def isPalindrome(self, head: ListNode | None) -> bool:
+        fast, slow = head, head
+        slow_prv   = None
+
+        # Find the middle of the linked list
+        while fast and fast.next:
+            fast = fast.next.next
+            slow_prv = slow
+            slow = slow.next
+
+        # Decide whether to ignore middle value
+        if slow.next is not None and fast is not None:
+            slow = slow.next
+
+        # Split the list
+        if slow_prv and slow_prv.next is not None:
+            slow_prv.next = None
+
+        # Reverse the 2nd half of the list   
+        curr, prv = slow, None
+        while curr:
+            nxt = curr.next
+            curr.next = prv
+            prv = curr
+            curr = nxt
+
+        # Compare values in the lists
+        start, mid = head, prv
+        while start and mid:
+            if start.val != mid.val:
+                return False
+            
+            start = start.next
+            mid = mid.next
+
+        # Check for leftover nodes
+        if start or mid:
+            return False
+
+        return True
+
+    def _isPalindrome(self, head: ListNode | None) -> bool:
         stack = list()
         fast, slow = head, head
 
